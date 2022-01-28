bd0aef256e741499912b5ed979409b147b8d4775
Rafail Miliopoulos <rafail@rafail-macbook.home>
2022-01-28 11:04:03 +0100

    styling change

--- components/FilterActions.vue
+++ components/FilterActions.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="flex flex-col justify-between md:ml-18 md:mr-18 lg:ml-16 lg:mr-16 xl:ml-4 xl:mr-4 lg:flex-row">
+<div class="flex flex-col justify-between md:ml-18 md:mr-18 lg:ml-22 lg:mr-22 xl:ml-4 xl:mr-4 lg:flex-row">
   <div class="flex search-input self-center h-12 w-60 p-2 border-2 rounded">
     <span class="inline-flex"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
